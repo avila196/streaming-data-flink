7a9d9a3b7f58f344e051713e8dd77a9a13f659c2
David Avila <davidavila@Factored-C02D9TK1ML7H.local>
2021-06-23 15:42:01 -0500

    fixed offensive parsing and updated maven pom file for java 8

--- trending/src/main/java/trending/TrendingTopics.java
+++ trending/src/main/java/trending/TrendingTopics.java
@@ -1,24 +1,23 @@
 package trending;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.flink.util.Collector;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.core.fs.FileSystem.WriteMode;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
 
 //Class that handles all input text from Kafka and outputs the trending words and phrases cleaned
@@ -40,26 +39,32 @@ public class TrendingTopics {
 	public static void main(String[] args) throws Exception {
 		//Get execution environment object
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
 
 		//Create DataStream for stop words and get its respective BroadcastStream object
-		DataStream<String> stopWords = env.readTextFile("./data/stop_words.txt");
+		DataStream<String> stopWords = env.readTextFile("/Users/davidavila/Documents/github_repos/"
+				+ "streaming-data-flink/trending/data/stop_words.txt");
 		BroadcastStream<String> stopWordsBroadcast = stopWords.broadcast(stopWordsDescriptor);
 
 		//Create DataStream for offensive words and get its respective BroadcastStream object
-		DataStream<String> offensiveWords = env.readTextFile("./data/offensive_words.txt");
+		DataStream<String> offensiveWords = env.readTextFile("/Users/davidavila/Documents/github_repos/"
+				+ "streaming-data-flink/trending/data/offensive_words.txt");
 		BroadcastStream<String> offensiveWordsBroadcast = offensiveWords.broadcast(offensiveWordsDescriptor);
 
 		//Start listening to Kafka server for all input data. The code parses the data as it comes,
 		//removing all punctuation and splitting it
 		Properties prop = new Properties();
-		prop.setProperty("bootstrap.servers", "127.0.0.1:9092");
-		DataStream<ArrayList<String>> kafkaData = env.addSource(new FlinkKafkaConsumer011<String>("data", new SimpleStringSchema(), prop))
+		prop.setProperty("bootstrap.servers", "localhost:9092");
+		DataStream<ArrayList<String>> kafkaData = env.addSource(new FlinkKafkaConsumer<String>("test", new SimpleStringSchema(), prop))
 				.map(new MapFunction<String, ArrayList<String>>() {
 					public ArrayList<String> map(String text) {
 						//We filter out the words by removing all non-alphabetic chars, making all lower-case
 						//and splitting by any whitespace to get all words
-						return (ArrayList<String>)Arrays.asList(text.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+"));
+						String[] words = text.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+						ArrayList<String> wordsList = new ArrayList<>();
+						for(String word : words) {
+							wordsList.add(word);
+						}
+						return wordsList;
 					}
 				});
 
@@ -71,26 +76,17 @@ public class TrendingTopics {
 		//(2) Check against offensive words and get just the offensive ones
 		DataStream<ArrayList<String>> offensiveWordsFounds = reducedWords
 				.connect(offensiveWordsBroadcast)
-				.process(new OffensiveWordsCheck());
+				.process(new OffensiveWordsCheck(true));
+		
+		//(3) Check against offensive words and get non-offensive ones
+		reducedWords = reducedWords
+				.connect(offensiveWordsBroadcast)
+				.process(new OffensiveWordsCheck(false));
 
-		//(3) Map all original words to categories and aggregate on the categories counts (adding them) to find the 
+		//(4) Map all original words to categories and aggregate on the categories counts (adding them) to find the 
 		//trending topics. The categories are found using a pre-trained ML classification model. A sliding window
 		//is used with 60 sec size and 2 sec slide time.
-		//TODO!
-
-		//(4) Map all reduced words to counts and aggregate on the counts (adding them) to find the most common ones.
-		//This is to find the meaningful trending words. A sliding window is used with 60 sec size and 2 sec slide time
-		DataStream<Tuple2<String, Integer>> trendingWords = reducedWords
-				.flatMap(new FlatMapFunction<ArrayList<String>, Tuple2<String, Integer>>() {
-					public void flatMap(ArrayList<String> words, Collector<Tuple2<String, Integer>> out) {
-						//Loop through all reduced words and map them to counts of 1
-						for(String word : words) {
-							out.collect(new Tuple2<String, Integer>(word, 1));
-						}
-					}
-				}).keyBy(0)
-				.window(SlidingProcessingTimeWindows.of(Time.seconds(60), Time.seconds(2)))
-				.sum(1);
+		//TODO! Get topic/category using kafkaData, original cleaned data!
 
 		//(5) Map all offensive words to counts and aggregate on the counts (adding them) to find the most common ones.
 		//This is to find the most common offensive words. A sliding window is used with 60 sec size and 2 sec slide time
@@ -102,15 +98,34 @@ public class TrendingTopics {
 							out.collect(new Tuple2<String, Integer>(word, 1));
 						}
 					}
-				}).keyBy(0)
+				}).keyBy(word -> word.f0)
 				.window(SlidingProcessingTimeWindows.of(Time.seconds(60), Time.seconds(2)))
 				.sum(1);
 
-		//(5) Output all results found:
+		//(6) Map all reduced words to counts and aggregate on the counts (adding them) to find the most common ones.
+		//This is to find the meaningful trending words. A sliding window is used with 60 sec size and 2 sec slide time
+		DataStream<Tuple2<String, Integer>> trendingWords = reducedWords
+				.flatMap(new FlatMapFunction<ArrayList<String>, Tuple2<String, Integer>>() {
+					public void flatMap(ArrayList<String> words, Collector<Tuple2<String, Integer>> out) {
+						//Loop through all reduced words and map them to counts of 1
+						for(String word : words) {
+							out.collect(new Tuple2<String, Integer>(word, 1));
+						}
+					}
+				}).keyBy(word -> word.f0)
+				.window(SlidingProcessingTimeWindows.of(Time.seconds(60), Time.seconds(2)))
+				.sum(1);
+
+		//(6) Output all results found:
 		//   - Trending topics (categories)
 		//   - Trending words
 		//   - Trending offensive words
 		//TODO!
+
+		trendingWords.writeAsText("/Users/davidavila/Documents/github_repos/"
+				+ "streaming-data-flink/trending/results/t_words.txt", WriteMode.OVERWRITE);
+		mostCommonOffensive.writeAsText("/Users/davidavila/Documents/github_repos/"
+				+ "streaming-data-flink/trending/results/o_words.txt", WriteMode.OVERWRITE);
 
 		// execute program
 		env.execute("Streaming Trending Topics!");
@@ -156,26 +171,43 @@ public class TrendingTopics {
 	@SuppressWarnings("serial")
 	//Class that broadcasts all input words against offensive words
 	public static class OffensiveWordsCheck extends BroadcastProcessFunction<ArrayList<String>, String, ArrayList<String>> {
+
+		//Boolean field that represents if offensive or non-offensive should be collected
+		boolean collectOffensive;
+
+		/**
+		 * Constructor of the class. Receives a boolean representing if offensive or non-offensive should be collected
+		 * @param collectOffensive
+		 */
+		public OffensiveWordsCheck(boolean collectOffensive) {
+			this.collectOffensive = collectOffensive;
+		}
+
 		/**
 		 * Processes the given non-broadcast element (list of words)
 		 */
 		public void processElement(ArrayList<String> words, ReadOnlyContext ctx, Collector<ArrayList<String>> out) throws Exception {
-			//We start looping through all offensive words and we create a new ArrayList to store all words found
-			ArrayList<String> offensive = new ArrayList<String>();
-			for (Map.Entry<String, String> stopWord: ctx.getBroadcastState(offensiveWordsDescriptor).immutableEntries()) {
-				//Get next offensive word and check if any of the input words is equal
-				String word = stopWord.getKey();
-				for(String inputWord : words) {
-					if(word.equals(inputWord)) {
-						//The word is added to list of offensive words found
-						offensive.add(word);
+			//We start looping through all input words and we create a new ArrayList to store all words to collect
+			ArrayList<String> toCollect = new ArrayList<String>();
+			for (String inputWord : words) {
+				//Check if current word is equal to any of the offensive words
+				boolean isOffensive = false;
+				for(Map.Entry<String, String> offensiveWord : ctx.getBroadcastState(offensiveWordsDescriptor).immutableEntries()) {
+					if(offensiveWord.getKey().equals(inputWord)) {
+						//The current word is an offensive word
+						isOffensive = true;
 						break;
 					}
 				}
+				//Now, we collect the word if it matches the condition needed
+				if(isOffensive && collectOffensive) {
+					toCollect.add(inputWord);
+				} else if(!isOffensive && !collectOffensive) {
+					toCollect.add(inputWord);
+				}
 			}
-			//Once the loop ends, the list "words" contains all non offensive words and the list "offensive" contains
-			//all offensive words found, this is the one we need to return
-			out.collect(offensive);
+			//Once the loop ends, the list "nonStopWords" contains all non stop words
+			out.collect(toCollect);
 		}
 
 		/**
@@ -183,7 +215,7 @@ public class TrendingTopics {
 		 */
 		public void processBroadcastElement(String word, Context ctx, Collector<ArrayList<String>> out) throws Exception {
 			//We just put the word inside the broadcast state
-			ctx.getBroadcastState(stopWordsDescriptor).put(word, word);
+			ctx.getBroadcastState(offensiveWordsDescriptor).put(word, word);
 		} 
 	}
 }
